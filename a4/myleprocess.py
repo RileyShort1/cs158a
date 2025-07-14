47943755b7758dc36469d84f65d19dcf216bc79a
Riley <rileyshort1@gmail.com>
2025-07-13 23:41:46 -0700

    additions to a4

--- a4/myleprocess.py
+++ a4/myleprocess.py
@@ -1,3 +1,4 @@
+import queue
 import socket
 import threading
 import uuid
@@ -5,6 +6,7 @@ import csv
 import json
 import time
 import sys
+from queue import Queue
 
 # default looks for config file in working directory
 config_file_path = "config.txt"
@@ -22,22 +24,25 @@ with open(config_file_path, "r") as file:
     config = list(reader)
 
 # init global connection info with config file info
-SERVER = config[0][0]
+SERVER_IP = config[0][0]
 SERVER_PORT = config[0][1]
-CLIENT_SERVER = config[1][0]
-CLIENT_PORT = config[1][1]
+CLIENT_CONN_IP = config[1][0]
+CLIENT_CONN_PORT = config[1][1]
 
-# flag to wait on client connection
+# Server will place all messages here
+# Queue has built in thread safety
+MESSAGES = Queue()
+
+# flag for client and server connection status
 client_connected = threading.Event()
+server_connected = threading.Event()
 
-# global client connection init to null
-CLIENT = None
+leader_found = threading.Event()
+
+log_file = open(log_file_path, "r")
 
 
 class Message:
-    uuid = uuid.uuid4()
-    flag = 0
-
     # converts obj to JSON string with newline termination
     def convert_to_json(self):
         return json.dumps({
@@ -45,127 +50,119 @@ class Message:
             "flag": str(self.flag)
         }) + "\n"
 
-    def print_message(self):
-        return "UUID: " + str(self.uuid) + ", Flag: " + str(self.flag)
+    def to_string(self):
+        return "uuid: " + str(self.uuid) + ", flag: " + str(self.flag)
 
-def json_string_to_message_obj(json_string):
-    message = Message()
-    message_data = json.loads(json_string)
-    message.uuid = uuid.UUID(message_data["uuid"])
-    message.flag = int(message_data["flag"])
-    return message
+    def __init__(self, json_string=None):
+        if json_string is None:
+            self.uuid = uuid.uuid4()
+            self.flag = 0
+        else:
+            message_data = json.loads(json_string)
+            self.uuid = uuid.UUID(message_data["uuid"])
+            self.flag = int(message_data["flag"])
 
 
 NODE_INFO = Message() # our nodes id info
 
 
-# message sender function
+# sends message obj using socket
 def send_message(client_socket, message):
     try:
         client_socket.sendall(message.convert_to_json().encode())
+        log_file.write("Sent: " + message.to_string() + "\n")
     except OSError:
         print("Message Send Error")
 
 
-# connects with client and sets global connection variable
-def run_client():
-    global CLIENT
-    # establish connection with other server
-    try:
-        CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        CLIENT.connect((CLIENT_SERVER, int(CLIENT_PORT)))
-    except OSError as e:
-        print(f"Client Connection Error: [Errno {e.errno}] {e.strerror} - {e}")
-        return
-    client_connected.set() # set flag confirming client connection success
-
-
-def run_server():
-    global SERVER
+# starts server and begins placing incoming messages into queue
+def accumulate_messages():
+    global SERVER_IP
     global SERVER_PORT
-    global CLIENT
+    global MESSAGES
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((SERVER, int(SERVER_PORT)))
+    server.bind((SERVER_IP, int(SERVER_PORT)))
     server.listen()
 
-    # accept one connection
-    connection, address = server.accept()
-
-    # wait for client to confirm connection before proceeding
-    while not client_connected.is_set():
-        pass
-
-    log_file = open(log_file_path, "a")
-    log_file.write("Sent: " + str(NODE_INFO.uuid) + ", flag=" + str(NODE_INFO.flag) + ", " + str(NODE_INFO.flag) + "\n")
-    # send our id
-    send_message(CLIENT, NODE_INFO)
-
-    leader_id = None
+    connection, address = server.accept() # accept one connection
+    server_connected.set() # set flag to true
 
     buffer = bytes()
 
-    while True:
-        data = connection.recv(1024)
-        if not data:
-            return # connection has been closed
-        buffer += data # append data to buffer
-
-        while b'\n' in buffer:
-            # extract message data from buffer up to newline char
-            message, buffer = buffer.split(b'\n', 1) # does not include newline
-            # convert message to Message object
-            received_message = json_string_to_message_obj(message.decode())
-
-            if received_message.flag == 1:
-                # leader has already been decided
-                log_file.write("Received: uuid=" + str(received_message.uuid) + ", flag=" + str(received_message.flag) + ", " + str(NODE_INFO.flag) + "\n")
-                log_file.write("Leader is decided to " + str(received_message.uuid) + "\n")
-                NODE_INFO.flag = 1 # mark as leader found
-                send_message(CLIENT, received_message)
-                leader_id = str(received_message.uuid)
-                log_file.write("Leader is " + leader_id + "\n")
-                connection.close()
-                log_file.close()
-                return
+    while server_connected.is_set():
+        try:
+            data = connection.recv(1024)
+        except socket.error:
+            print("Error receiving data in accumulate_messages() - server connection terminated")
+            server_connected.clear() # set flag to false
+            connection.close()
+            break
+        if not data: # connection has been closed
+            server_connected.clear() # set flag to false
+            connection.close()
+        else:
+            buffer += data # append data to buffer
+            while b'\n' in buffer: # extract message data (does not include \n
+                message, buffer = buffer.split(b'\n', 1)
+                MESSAGES.put(Message(message.decode())) # enqueue message
 
 
-            if received_message.uuid > NODE_INFO.uuid:
-                log_file.write("Received: uuid=" + str(received_message.uuid) + ", flag=" + str(received_message.flag) + ", greater, " + str(NODE_INFO.flag) + "\n")
-                log_file.write("Sent: uuid=" + str(received_message.uuid) + ", flag=" + str(received_message.flag) + ", greater, " + str(NODE_INFO.flag) + "\n")
-                send_message(CLIENT, received_message) # forward message
+# function processes message obj according to LE algorithm
+def process_messages(client, message):
+    
+    if message.flag == 1: # leader has already been decided
+        NODE_INFO.flag = 1 # mark as leader found
+        send_message(client, message)
+        leader_found.set()
+        leader_id = str(message.uuid)
+        return
 
-            elif received_message.uuid == NODE_INFO.uuid:
-                # we are the leader
-                log_file.write("Leader is decided to " + str(NODE_INFO.uuid) + "\n")
-                NODE_INFO.flag = 1 # mark as leader found
-                log_file.write("Sent=" + str(NODE_INFO.uuid) + ", flag=" + str(NODE_INFO.flag) + ", equal, " + str(NODE_INFO.flag) + "\n")
-                send_message(CLIENT, NODE_INFO)
-                leader_id = str(NODE_INFO.uuid)
-                log_file.write("Leader is " + leader_id + "\n")
-                log_file.close()
-                connection.close()
-                return
-            else:
-                log_file.write("Ignored: uuid=" + str(received_message.uuid) + ", flag=" + str(received_message.flag) + " less, " + str(NODE_INFO.flag) + "\n")
+    if message.uuid > NODE_INFO.uuid:
+        send_message(client, message) # forward message
+
+    elif message.uuid == NODE_INFO.uuid:
+        # we are the leader
+        NODE_INFO.flag = 1
+        send_message(client, NODE_INFO)
+        leader_found.set()
+        leader_id = str(NODE_INFO.uuid)
+        return
+    else:
+        print("Ignored: " + message.to_string() + " less, " + str(NODE_INFO.flag) + "\n")
 
 
+# connects with client and sets global connection variable
+def run_client(delay):
+    time.sleep(delay)
+    try: # establish connection with other server
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((CLIENT_CONN_IP, int(CLIENT_CONN_PORT)))
+    except OSError as e:
+        print("Client Connection Error in run_client()")
+        return
+    client_connected.set() # set flag
 
+    # get message and process
+    while not leader_found.is_set():
+        try:
+            message = MESSAGES.get(block=False)
+            process_messages(client, message)
+        except queue.Empty:
+            continue
+
+    client_connected.clear()
+    client.close()
 
 # start server in thread
-server_thread = threading.Thread(target=run_server)
+server_thread = threading.Thread(target=accumulate_messages())
 server_thread.start()
 
-time.sleep(5) # wait 5 seconds before trying to connect client
-
-client_thread = threading.Thread(target=run_client)
+# start client
+client_thread = threading.Thread(target=run_client, args=(5,))
 client_thread.start()
 
 client_thread.join()
 server_thread.join()
-
-if client_connected.is_set():
-    CLIENT.close()
-
 
 
